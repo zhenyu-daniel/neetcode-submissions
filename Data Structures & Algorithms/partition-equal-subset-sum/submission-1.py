263461b4fb0d8c65845fa7b21af7828899cab702
Zhenyu Dai <84886864+zhenyu-daniel@users.noreply.github.com>
2026-07-23 16:59:46 -0500

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums)%2 ==1:
+            return False
+
+        dp = set()
+        dp.add(0)
+        target = sum(nums)/2
+
+        for i in range(len(nums) -1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        
+        if target in dp:
+            return True
+        else:
+            return False
